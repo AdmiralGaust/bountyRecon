64c4fb8d88671a055e1bf3ec935783af6e8186ce
abmp <abc@example.com>
2019-11-24 19:19:45 +0530

    Done subdomain enumeration and httprobe, massdns

--- bountyRecon.sh
+++ bountyRecon.sh
@@ -29,14 +29,44 @@ mkdir -p "${Recon_Home}/logs"
 
 logfile="${Recon_Home}/logs/${start_time}.log"
 
-######Subdomain Enumeration
+
+
+######Subdomain Enumeration########
 
 mkdir -p "${Recon_Home}/subdomains"
 
-echo -e "\n[!] Started Subdomain Enumeration at ${start_time}\n"| tee -a ${logfile}
+echo -e "\n[+] Started Subdomain Enumeration at ${start_time}\n"| tee -a ${logfile}
 
 ./subdomains.sh $domains "${Recon_Home}/subdomains" "${amass_config_path}"
 
-echo -e "[+] Subdomain Enumeration Finished at `date '+%d%m%y_%H%M%S'`"| tee -a ${logfile}
+echo -e "[+] Subdomain Enumeration Finished at `date '+%d%m%y_%H%M%S'`\n"| tee -a ${logfile}
 
-####
+cat "${Recon_Home}/subdomains/subfinder.txt" "${Recon_Home}/subdomains/amass.txt" |sed  "/^[\.*]/d" |sort -u > "${Recon_Home}/subdomains/subdomains.txt"
+
+
+
+########Testing for Alive and Resolvable domains####
+
+echo "[+] Checking for alive domains..\n" | tee -a ${logfile}
+
+cat "${Recon_Home}/subdomains/subdomains.txt" | httprobe -p http:8080 https:8080 https:8443 http:8000 https:8000 -c 50| tee -a "${Recon_Home}/subdomains/alive.txt"
+
+echo "[+] Finished Checking Alive domains\n" | tee -a ${logfile}
+
+massdns_home=$(getValueFromConfig "massdns_home")
+
+echo "[+] Checking for Resolvable domains..\n" | tee -a ${logfile}
+
+${massdns_home}/massdns -r ${massdns_home}/lists/resolvers.txt -o S -w "${Recon_Home}/subdomains/massdns.txt" "${Recon_Home}/subdomains/subdomains.txt"
+
+echo "[+] Finished Checking Resolvable domains\n" | tee -a ${logfile}
+
+cat "${Recon_Home}/subdomains/massdns.txt" |cut -d " " -f 1|sed "s/\.$//"|sort -u > "${Recon_Home}/subdomains/resolvable.txt"
+
+
+
+##########Screenshot the target with eyewitness######
+
+
+
+
--- test.sh
+++ test.sh
@@ -1,6 +1,3 @@
-function getValueFromConfig() {
-    echo `grep ${1} config.conf | cut -d '=' -f 2`
-}
-
-a=`getValueFromConfig "Recon_Home"`
-echo `pwd`/${1}
+cmd1="pwd"
+cmd2="ls"
+parallel
