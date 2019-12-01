b6dbd61597568f7e9e8cf38e049e7776032d0ca2
abmp <abc@example.com>
2019-12-01 21:11:38 +0530

    amass added

--- subdomains.sh
+++ subdomains.sh
@@ -1,7 +1,14 @@
 
+function getValueFromConfig() {
+    echo `grep ${1} config.conf | cut -d '=' -f 2`
+}
+
+
+amass_home=$(getValueFromConfig "amass_home") 
+
 ##Subfinder
 subfinder -dL ${1} -o "${2}/subfinder.txt" -t 100 -recursive 
 
 ##Amass
-amass enum -config "${3}" -o "${2}/amass.txt" -df ${1}
+${amass_home}/amass enum -config "${3}" -o "${2}/amass.txt" -df ${1}
 
